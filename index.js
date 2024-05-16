a2281058f432e7c74cb4e6cfecebc1e11854ecc1
dilpr <dilpreetbajwa1@gmail.com>
2024-05-15 23:16:15 -0400

    commit

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const app = express();
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const connect = async () => {
+    try {
+      await mongoose.connect(process.env.MONGOBD_URI);
+      console.log("Connected to mongoDB!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  app.get('/')
 // Define routes and middleware here
 // ...
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+    connect();
     console.log(`Server running on port ${PORT}`);
 });
